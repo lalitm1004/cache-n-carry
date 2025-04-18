649d02c58359726193181dd421a7a2834d3a467d
lalitm1004 <lalitm1004@gmail.com>
2025-04-18 13:52:03 +0530

    feat: bring schema to date

---
+++ prisma/migrations/20250418081756_create_tables/migration.sql
@@ -0,0 +1,150 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `user_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `staff` (
+    `id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `student` (
+    `id` VARCHAR(191) NOT NULL,
+    `rollNumber` VARCHAR(12) NOT NULL,
+    `current_room_id` VARCHAR(191) NOT NULL,
+    `next_room_id` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `student_rollNumber_key`(`rollNumber`),
+    UNIQUE INDEX `student_current_room_id_key`(`current_room_id`),
+    UNIQUE INDEX `student_next_room_id_key`(`next_room_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `hostel` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `room` (
+    `id` VARCHAR(191) NOT NULL,
+    `number` VARCHAR(6) NOT NULL,
+    `hostel_id` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `room_hostel_id_number_key`(`hostel_id`, `number`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `warehouse` (
+    `id` VARCHAR(191) NOT NULL,
+    `location` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `belonging` (
+    `id` VARCHAR(191) NOT NULL,
+    `is_checked_in` BOOLEAN NOT NULL DEFAULT false,
+    `student_id` VARCHAR(191) NOT NULL,
+    `warehouse_id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `luggage` (
+    `id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `mattress` (
+    `id` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `session` (
+    `id` VARCHAR(191) NOT NULL,
+    `open_time` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `close_time` DATETIME(3) NULL,
+    `terminated` BOOLEAN NOT NULL DEFAULT false,
+    `staff_id` VARCHAR(191) NOT NULL,
+    `student_id` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `session_staff_id_student_id_key`(`staff_id`, `student_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `incident` (
+    `id` VARCHAR(191) NOT NULL,
+    `found_by` VARCHAR(191) NOT NULL,
+    `belongs_to` VARCHAR(191) NOT NULL,
+    `mattress_id` VARCHAR(191) NOT NULL,
+    `resolved` BOOLEAN NOT NULL DEFAULT false,
+    `openTime` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `closeTime` DATETIME(3) NULL,
+
+    UNIQUE INDEX `incident_found_by_key`(`found_by`),
+    UNIQUE INDEX `incident_belongs_to_key`(`belongs_to`),
+    UNIQUE INDEX `incident_mattress_id_key`(`mattress_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `staff` ADD CONSTRAINT `staff_id_fkey` FOREIGN KEY (`id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `student` ADD CONSTRAINT `student_id_fkey` FOREIGN KEY (`id`) REFERENCES `user`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `student` ADD CONSTRAINT `student_current_room_id_fkey` FOREIGN KEY (`current_room_id`) REFERENCES `room`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `student` ADD CONSTRAINT `student_next_room_id_fkey` FOREIGN KEY (`next_room_id`) REFERENCES `room`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `room` ADD CONSTRAINT `room_hostel_id_fkey` FOREIGN KEY (`hostel_id`) REFERENCES `hostel`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `belonging` ADD CONSTRAINT `belonging_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `belonging` ADD CONSTRAINT `belonging_warehouse_id_fkey` FOREIGN KEY (`warehouse_id`) REFERENCES `warehouse`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `luggage` ADD CONSTRAINT `luggage_id_fkey` FOREIGN KEY (`id`) REFERENCES `belonging`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `mattress` ADD CONSTRAINT `mattress_id_fkey` FOREIGN KEY (`id`) REFERENCES `belonging`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `session` ADD CONSTRAINT `session_staff_id_fkey` FOREIGN KEY (`staff_id`) REFERENCES `staff`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `session` ADD CONSTRAINT `session_student_id_fkey` FOREIGN KEY (`student_id`) REFERENCES `student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `incident` ADD CONSTRAINT `incident_mattress_id_fkey` FOREIGN KEY (`mattress_id`) REFERENCES `mattress`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `incident` ADD CONSTRAINT `incident_found_by_fkey` FOREIGN KEY (`found_by`) REFERENCES `student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `incident` ADD CONSTRAINT `incident_belongs_to_fkey` FOREIGN KEY (`belongs_to`) REFERENCES `student`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
