5474d3bc675bb162466450bf540bd2e24fc25e74
Rachit Kumar <rachit.prodigy205@gmail.com>
2025-04-20 21:01:14 +0530

    fix(checkout): rename CheckOutRequestBody

--- src/routes/api/belonging/checkout/+server.ts
+++ src/routes/api/belonging/checkout/+server.ts
@@ -4,17 +4,17 @@ import { db } from "$lib/server/database/database";
 import { Prisma } from "@prisma/client";
 import type { Prisma as PrismaType } from "@prisma/client";
 
-interface CheckInRequestBody {
+interface CheckOutRequestBody {
     belongingId: string;
     warehouseName: string;
     staffEmail: string;
 }
 
 export const POST: RequestHandler = async ({ request }) => {
-    let requestData: CheckInRequestBody;
+    let requestData: CheckOutRequestBody;
 
     try {
-        requestData = (await request.json()) as CheckInRequestBody;
+        requestData = (await request.json()) as CheckOutRequestBody;
     } catch (e) {
         throw error(400, { message: "invalid json body" });
     }
