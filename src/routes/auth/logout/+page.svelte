bd13924d363fbfbb9c31315a1b97fc7cb34e326e
lalitm1004 <lalitm1004@gmail.com>
2025-04-21 11:43:34 +0530

    feat: finish raw sql

---
+++ src/routes/auth/logout/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import { goto } from "$app/navigation";
+import { setUser } from "$lib/stores/UserStore";
+</script>
+
+<main class={`h-screen w-screen grid place-items-center`}>
+    <button class={`bg-red-800 text-white px-4 py-2`}
+        onclick={() => {
+            setUser(null);
+            goto('/auth/register')
+        }}>
+        Log Out
+    </button>
+    <a href="/">Home</a>
+</main>
